9f06d8c18db01569dc723512adae58778b0ddf1a
Emily <emilypearlasaro@gmail.com>
2021-03-11 13:52:41 -0500

    fixed syntax errors in server.js, but still not connect to the db

--- db/seed.js
+++ db/seed.js
@@ -1,68 +1,68 @@
-// const { Customer, Month, Order, db } = require('./index');
-const { DataTypes,  Model, Sequelize } = require('sequelize');
-const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:3000/kb_creamery_2', { logging: false });
+const { Customer, Month, Order, db } = require('./index');
+// const { DataTypes,  Model, Sequelize } = require('sequelize');
+// const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:3000/kb_creamery_2', { logging: false });
 
-class Customer extends Model {};
+// class Customer extends Model {};
 
-Customer.init({
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    validate: {
-      notEmpty: false
-    }
-  },
-  email: {
-    type: DataTypes.STRING,
-    validate: {
-      isEmail: true
-    }
-  }
-},
-  { sequelize: db, modelName: 'Customer' }
-);
+// Customer.init({
+//   name: {
+//     type: DataTypes.STRING,
+//     allowNull: false,
+//     validate: {
+//       notEmpty: false
+//     }
+//   },
+//   email: {
+//     type: DataTypes.STRING,
+//     validate: {
+//       isEmail: true
+//     }
+//   }
+// },
+//   { sequelize: db, modelName: 'Customer' }
+// );
 
-class Month extends Model {};
+// class Month extends Model {};
 
-Month.init({
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  flavor1: {
-    type: DataTypes.STRING,
-  },
-  flavor2: {
-    type: DataTypes.STRING,
-  }
-},
-  { sequelize: db, modelName: 'Month' }
-);
+// Month.init({
+//   name: {
+//     type: DataTypes.STRING,
+//     allowNull: false
+//   },
+//   flavor1: {
+//     type: DataTypes.STRING,
+//   },
+//   flavor2: {
+//     type: DataTypes.STRING,
+//   }
+// },
+//   { sequelize: db, modelName: 'Month' }
+// );
 
-class Order extends Model {};
+// class Order extends Model {};
 
-Order.init({
-  orderPlaced: {
-    type: DataTypes.BOOLEAN,
-  }
-},
-  { sequelize: db, modelName: 'Order' }
-);
+// Order.init({
+//   orderPlaced: {
+//     type: DataTypes.BOOLEAN,
+//   }
+// },
+//   { sequelize: db, modelName: 'Order' }
+// );
 
-Order.belongsTo(Month);
-Order.belongsTo(Customer);
+// Order.belongsTo(Month);
+// Order.belongsTo(Customer);
 
-// check on the type of magic method for returning both month and customer information
-Order.findMonths = function (id) {
-  return this.findOnce({
-    include: {
-      model: Month,
-    },
-    where: {
-      id
-    }
-  })
-}
+// // check on the type of magic method for returning both month and customer information
+// Order.findMonths = function (id) {
+//   return this.findOnce({
+//     include: {
+//       model: Month,
+//     },
+//     where: {
+//       id
+//     }
+//   })
+// }
 
 
 
--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-const syncAndSeed = require('./db/seed');
-const db = require('./db/index');
+const { syncAndSeed, db } = require('./db/seed');
+// const { db }= require('./db/index');
 const app = require('./api/app.js');
 const port = process.env.PORT || 3000;
 
 // app.use('/api', require('./api/app'));
-// app.use('/api', require('./routes/index'));
 
 const init = async () => {
   try {
